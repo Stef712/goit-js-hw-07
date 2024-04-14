0682ee1c97f21146396482737e16ad4878523509
Stef712 <148722481+Stef712@users.noreply.github.com>
2024-04-14 15:27:35 +0300

    first-exercise-finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryIndex = document.querySelector(".gallery");
+
+function createImage() {
+  let galleryImages = "";
+  for (let i = 0; i < galleryItems.length; i++) {
+    galleryImages += `<li class = "gallery__item">
+          <a class = "gallery__link" href = "${galleryItems[i].original}">
+            <img class = "gallery__image"
+                src = "${galleryItems[i].preview}"
+                data-source = "${galleryItems[i].original}"
+                alt = "${galleryItems[i].description}"
+            />
+          </a >
+        </li>`;
+  }
+  galleryIndex.innerHTML = galleryImages;
+}
+createImage();
+
+galleryIndex.addEventListener("click", itemSelected);
+
+function itemSelected(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src = '${event.target.dataset.source}'>`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", pressEsc);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", pressEsc);
+      },
+    }
+  );
+  instance.show();
+
+  function pressEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
